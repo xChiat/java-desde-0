24cac2679d1fb818b13ffd001252a82d391fe397
Cristian <chiat.contact@gmail.com>
2024-01-01 14:44:00 -0300

    primeras clases
    videos del 1-5

---
+++ suma.java
@@ -0,0 +1,9 @@
+public class suma{
+  public static void main(String args[]){
+   int num_1  = 5;
+   int num_2  = 2;
+   int result = 0;
+   result = num_1 + num_2;
+   System.out.println("El valor de tu suma es: "+result);
+ }
+}
